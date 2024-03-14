509d8e9a92857aafe09d482146cba06ec147c06d
Mark Peschel <mpeschel10@gmail.com>
2024-03-14 17:20:44 -0400

    Implement account creation and deletion. Add basic test. Largely untested, and will remain so because I don't have time.

--- server.mjs
+++ server.mjs
@@ -45,6 +45,54 @@ async function verifyIdToken(token) {
     }
 }
 
+async function createUser(newUser) {
+    if (newUser === undefined) return [ new Error('user must be defined.'), undefined];
+    if (newUser.email === undefined) return [ new Error('user.email must be defined.'), undefined];
+    if (newUser.password === undefined) return [ new Error('user.password must be defined.'), undefined];
+
+    const validatedUser = {};
+    for (const paramName of ['email', 'emailVerified', 'phoneNumber', 'password', 'displayName', 'photoURL', 'disabled' ]) {
+        validatedUser[paramName] = newUser[paramName];
+    }
+
+    const customClaim = {
+        roles: ['c']
+    };
+    if (newUser.roles) {
+        if (newUser.roles.length !== 1) {
+            return [ new Error('user.roles, if present, must have length exactly 1.')]
+        }
+        if (newUser.roles[0] !== 'c' && newUser.roles[0] !== 'p') {
+            return [ new Error('user.roles, if present, must contain only \'c\' or \'p\'.')]
+        }
+        customClaim.roles = newUser.roles;
+    }
+    
+    try {
+        const userRecord = await auth.createUser(validatedUser);
+        await auth.setCustomUserClaims(userRecord.uid, customClaim);
+
+        return [ undefined, userRecord ];
+    } catch (error) {
+        return [ error, undefined ];
+    }
+}
+
+async function deleteUser(uid, email) {
+    try {
+        if (uid === undefined) {
+            if (email === undefined) {
+                return [ new Error("At least one of uid and email must be defined."), undefined ];
+            }
+            uid = (await auth.getUserByEmail(email)).uid;
+        }
+        await auth.deleteUser(uid);
+        return [ undefined, uid ];
+    } catch (error) {
+        return [ error, uid ];
+    }
+}
+
 async function POST_users_json(req, res) {
     let bodyString;
     try {
@@ -55,11 +103,11 @@ async function POST_users_json(req, res) {
         return;
     }
 
-    let token, user;
+    let token, newUser;
     try {
         const bodyJSON = JSON.parse(bodyString);
         token = bodyJSON.token;
-        user = bodyJSON.user;
+        newUser = bodyJSON.user;
     } catch (error) {
         res.statusCode = 400;
         res.end("Body of request should be JSON object with {token: idToken, user: newUser}.");
@@ -85,13 +133,67 @@ async function POST_users_json(req, res) {
         return;
     }
 
-    console.log('Creating new account', user);
-    if (user === undefined) {
+    const [ createError, userRecord ] = await createUser(newUser);
+    if (createError) {
         res.statusCode = 400;
-        res.end("Body of request should be JSON object with {token: idToken, user: newUser}, where const idToken = signInWithEmailAndPassword(...).user.getIdToken()");
+        res.end(createError.toString());
+        return;
+    }
+    console.log('New user', userRecord.uid);
+
+    res.statusCode = 200;
+    res.end(JSON.stringify(userRecord.uid));
+}
+
+async function DELETE_users_json(req, res) {
+    let bodyString;
+    try {
+        bodyString = await awfulReadAll(req);
+    } catch (error) {
+        res.statusCode = 500;
+        res.end();
+        return;
+    }
+
+    let token, uid, email;
+    try {
+        const bodyJSON = JSON.parse(bodyString);
+        token = bodyJSON.token;
+        uid = bodyJSON.user.uid;
+        email = bodyJSON.user.email;
+    } catch (error) {
+        res.statusCode = 400;
+        res.end("Body of request should be JSON object with user : {uid: ...} or user: {email:...}.");
+        return;
+    }
+
+    if (token === undefined) {
+        res.statusCode = 401;
+        res.end("Body of request should be JSON object with {token: idToken, user}, where const idToken = signInWithEmailAndPassword(...).user.getIdToken()");
         return;
     }
     
+    const [ authError, claims ] = await verifyIdToken(token);
+    if (authError) {
+        res.statusCode = 401;
+        res.end("Invalid token. Might be expired, or you might be uploading the wrong thing, or wrapping it in quotes, or something.");
+        return;
+    }
+
+    if (!claims.roles.includes('c')) {
+        res.statusCode = 403;
+        res.end("You must be a clinician to delete an account.");
+        return;
+    }
+
+    const [ deleteError, deletedUid ] = await deleteUser(uid, email);
+    if (deleteError) {
+        res.statusCode = 400;
+        res.end(deleteError.toString());
+        return;
+    }
+    console.log('Deleted user', deletedUid);
+
     res.statusCode = 204;
     res.end();
 }
@@ -100,6 +202,9 @@ async function users_json(req, res) {
     switch (req.method) {
         case 'POST':
             POST_users_json(req, res);
+            break;
+        case 'DELETE':
+            DELETE_users_json(req, res);
             break;
         default:
             res.statusCode = 405;
